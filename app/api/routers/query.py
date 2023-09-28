09b066b15a8c46f4fe654627b6ba69ce53590d81
rmanaem <rmanaem@protonmail.ch>
2023-09-28 16:43:06 -0400

    Cleaned the good stuff
    Co-authored-by: Sebastian Urchs <surchs@users.noreply.github.com>
    Co-authored-by: Alyssa Dai <alyssa.ydai@gmail.com>

--- app/api/routers/query.py
+++ app/api/routers/query.py
@@ -8,9 +8,6 @@ from ..models import QueryModel
 router = APIRouter(prefix="/query", tags=["query"])
 
 
-# TODO: update to change the logic once crud is modified
-
-
 @router.get("/")
 async def get_query(query: QueryModel = Depends(QueryModel)):
     """When a GET request is sent, return list of dicts corresponding to subject-level metadata aggregated by dataset."""
