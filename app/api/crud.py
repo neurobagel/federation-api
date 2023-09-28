596336945f54c4204b8da020d40d5cc95317e630
Sebastian Urchs <surchs@users.noreply.github.com>
2023-09-28 12:10:27 -0400

    [ENH] Very inspired first federation prototype (#1)
    * set up route and crud function for basic federation request
    
    Still the same
    
    * Yay, federation!
    
    Co-authored-by: Alyssa Dai <alyssa.ydai@gmail.com>
    Co-authored-by: rmanaem <rmanaem@protonmail.ch>

--- app/api/crud.py
+++ app/api/crud.py
@@ -1,13 +1,9 @@
 """CRUD functions called by path operations."""
 
-import os
-
 import httpx
-import pandas as pd
-from fastapi import HTTPException, status
+from fastapi import HTTPException
 
 from . import utility as util
-from .models import CohortQueryResponse
 
 # Order that dataset and subject-level attributes should appear in the API JSON response.
 # This order is defined explicitly because when graph-returned results are transformed to a dataframe,
@@ -67,129 +63,41 @@ async def get(
         Response of the POST request.
 
     """
-    try:
-        response = httpx.post(
-            url=util.QUERY_URL,
-            content=util.create_query(
-                return_agg=util.RETURN_AGG.val,
-                age=(min_age, max_age),
-                sex=sex,
-                diagnosis=diagnosis,
-                is_control=is_control,
-                min_num_sessions=min_num_sessions,
-                assessment=assessment,
-                image_modal=image_modal,
-            ),
-            headers=util.QUERY_HEADER,
-            auth=httpx.BasicAuth(
-                os.environ.get(util.GRAPH_USERNAME.name),
-                os.environ.get(util.GRAPH_PASSWORD.name),
-            ),
+    cross_node_results = []
+    params = {}
+    if min_age:
+        params["min_age"] = min_age
+    if max_age:
+        params["max_age"] = max_age
+    if sex:
+        params["sex"] = sex
+    if diagnosis:
+        params["diagnosis"] = diagnosis
+    if is_control:
+        params["is_control"] = is_control
+    if min_num_sessions:
+        params["min_num_sessions"] = min_num_sessions
+    if assessment:
+        params["assessment"] = assessment
+    if image_modal:
+        params["image_modal"] = image_modal
+
+    for node_url in util.NEUROBAGEL_NODES:
+        response = httpx.get(
+            url=node_url,
+            params=params,
             # TODO: Revisit timeout value when query performance is improved
             timeout=30.0,
         )
-    except httpx.ConnectTimeout as exc:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Timed out while connecting to the server. Please confirm that you are connected to the McGill network and try again.",
-        ) from exc
 
-    if not response.is_success:
-        raise HTTPException(
-            status_code=response.status_code,
-            detail=f"{response.reason_phrase}: {response.text}",
-        )
+        print("request was: ", response.request.url)
 
-    results = response.json()
-
-    results_dicts = [
-        {k: v["value"] for k, v in res.items()}
-        for res in results["results"]["bindings"]
-    ]
-    results_df = pd.DataFrame(results_dicts).reindex(columns=ATTRIBUTES_ORDER)
-
-    response_obj = []
-    dataset_cols = ["dataset_uuid", "dataset_name"]
-    if not results_df.empty:
-        for (dataset_uuid, dataset_name), group in results_df.groupby(
-            by=dataset_cols
-        ):
-            if util.RETURN_AGG.val:
-                subject_data = list(group["session_file_path"].dropna())
-            else:
-                subject_data = (
-                    group.drop(dataset_cols, axis=1)
-                    .groupby(by=["sub_id", "session_id"])
-                    .agg(
-                        {
-                            "sub_id": "first",
-                            "session_id": "first",
-                            "num_sessions": "first",
-                            "age": "first",
-                            "sex": "first",
-                            "diagnosis": lambda x: list(set(x)),
-                            "subject_group": "first",
-                            "assessment": lambda x: list(set(x)),
-                            "image_modal": lambda x: list(set(x)),
-                            "session_file_path": "first",
-                        }
-                    )
-                )
-                subject_data = list(subject_data.to_dict("records"))
-
-            response_obj.append(
-                CohortQueryResponse(
-                    dataset_uuid=dataset_uuid,
-                    dataset_name=dataset_name,
-                    dataset_portal_uri=group["dataset_portal_uri"].iloc[0]
-                    if group["dataset_portal_uri"].notna().all()
-                    else None,
-                    num_matching_subjects=group["sub_id"].nunique(),
-                    subject_data=subject_data,
-                    image_modals=list(group["image_modal"].unique()),
-                )
+        if not response.is_success:
+            raise HTTPException(
+                status_code=response.status_code,
+                detail=f"{response.reason_phrase}: {response.text}",
             )
 
-    return response_obj
+        cross_node_results.append(response.json())
 
-
-async def get_terms(data_element_URI: str):
-    """
-    Makes a POST request to Stardog API using httpx where the payload is a SPARQL query generated by the create_terms_query function.
-
-    Parameters
-    ----------
-    data_element_URI : str
-        Controlled term of neurobagel class for which all the available terms should be retrieved.
-
-    Returns
-    -------
-    httpx.response
-        Response of the POST request.
-    """
-    response = httpx.post(
-        url=util.QUERY_URL,
-        content=util.create_terms_query(data_element_URI),
-        headers=util.QUERY_HEADER,
-        auth=httpx.BasicAuth(
-            os.environ.get(util.GRAPH_USERNAME.name),
-            os.environ.get(util.GRAPH_PASSWORD.name),
-        ),
-    )
-
-    if not response.is_success:
-        raise HTTPException(
-            status_code=response.status_code,
-            detail=f"{response.reason_phrase}: {response.text}",
-        )
-
-    results = response.json()
-
-    results_dict = {
-        data_element_URI: [
-            result["termURL"]["value"]
-            for result in results["results"]["bindings"]
-        ]
-    }
-
-    return results_dict
+    return cross_node_results
--- app/main.py
+++ app/main.py
@@ -1,50 +1,33 @@
 """Main app."""
 
-import os
-import warnings
-
 import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
 
-from .api import utility as util
 from .api.routers import query
 
 app = FastAPI(default_response_class=ORJSONResponse)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=util.parse_origins_as_list(util.ALLOWED_ORIGINS.val),
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# TODO: bring this back when we spin up the query tool
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=util.parse_origins_as_list(util.ALLOWED_ORIGINS.val),
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
-@app.on_event("startup")
-async def auth_check():
-    """Checks whether username and password environment variables are set."""
-    if (
-        # TODO: Check if this error is still raised when variables are empty strings
-        os.environ.get(util.GRAPH_USERNAME.name) is None
-        or os.environ.get(util.GRAPH_PASSWORD.name) is None
-    ):
-        raise RuntimeError(
-            f"The application was launched but could not find the {util.GRAPH_USERNAME.name} and / or {util.GRAPH_PASSWORD.name} environment variables."
-        )
-
-
-@app.on_event("startup")
-async def allowed_origins_check():
-    """Raises warning if allowed origins environment variable has not been set or is an empty string."""
-    if os.environ.get(util.ALLOWED_ORIGINS.name, "") == "":
-        warnings.warn(
-            f"The API was launched without providing any values for the {util.ALLOWED_ORIGINS.name} environment variable. "
-            "This means that the API will only be accessible from the same origin it is hosted from: https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy. "
-            f"If you want to access the API from tools hosted at other origins such as the Neurobagel query tool, explicitly set the value of {util.ALLOWED_ORIGINS.name} to the origin(s) of these tools (e.g. http://localhost:3000). "
-            "Multiple allowed origins should be separated with spaces in a single string enclosed in quotes. "
-        )
+# @app.on_event("startup")
+# async def allowed_origins_check():
+#     """Raises warning if allowed origins environment variable has not been set or is an empty string."""
+#     if os.environ.get(util.ALLOWED_ORIGINS.name, "") == "":
+#         warnings.warn(
+#             f"The API was launched without providing any values for the {util.ALLOWED_ORIGINS.name} environment variable. "
+#             "This means that the API will only be accessible from the same origin it is hosted from: https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy. "
+#             f"If you want to access the API from tools hosted at other origins such as the Neurobagel query tool, explicitly set the value of {util.ALLOWED_ORIGINS.name} to the origin(s) of these tools (e.g. http://localhost:3000). "
+#             "Multiple allowed origins should be separated with spaces in a single string enclosed in quotes. "
+#         )
 
 
 app.include_router(query.router)
