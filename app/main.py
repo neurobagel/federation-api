92d2190c9b37dde669aaad73f0d064b5afc54977
Arman Jahanpour <77515879+rmanaem@users.noreply.github.com>
2023-11-15 13:19:51 -0500

    [ENH] Changed the favicon to neurobagel logo (#20)
    * Changed the favicon to neurobagel logo
    
    * Updated favicon_url

--- app/main.py
+++ app/main.py
@@ -3,11 +3,15 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import ORJSONResponse
+from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
+from fastapi.responses import ORJSONResponse, RedirectResponse
 
 from .api.routers import attributes, query
 
-app = FastAPI(default_response_class=ORJSONResponse)
+app = FastAPI(
+    default_response_class=ORJSONResponse, docs_url=None, redoc_url=None
+)
+favicon_url = "https://raw.githubusercontent.com/neurobagel/documentation/main/docs/imgs/logo/neurobagel_favicon.png"
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,6 +20,38 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    """
+    Overrides the default favicon with a custom one.
+    """
+    return RedirectResponse(url=favicon_url)
+
+
+@app.get("/docs", include_in_schema=False)
+def overridden_swagger():
+    """
+    Overrides the Swagger UI HTML for the "/docs" endpoint.
+    """
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",
+        title="Neurobagel Federation API",
+        swagger_favicon_url=favicon_url,
+    )
+
+
+@app.get("/redoc", include_in_schema=False)
+def overridden_redoc():
+    """
+    Overrides the Redoc HTML for the "/redoc" endpoint.
+    """
+    return get_redoc_html(
+        openapi_url="/openapi.json",
+        title="Neurobagel Federation API",
+        redoc_favicon_url=favicon_url,
+    )
 
 
 app.include_router(query.router)
