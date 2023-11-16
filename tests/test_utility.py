3fccba9deffd7cc9792cfee816157ec136c37768
rmanaem <rmanaem@protonmail.ch>
2023-11-16 16:28:17 -0500

    Updated tests

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -9,30 +9,35 @@ from app.api import utility as util
     "set_nodes, expected_nodes",
     [
         (
-            "https://firstnode.neurobagel.org/query",
-            ["https://firstnode.neurobagel.org/query/"],
+            "(https://firstnode.neurobagel.org/query, firstnode)",
+            {"https://firstnode.neurobagel.org/query/": "firstnode"},
         ),
         (
-            "https://firstnode.neurobagel.org/query/ https://secondnode.neurobagel.org/query",
-            [
-                "https://firstnode.neurobagel.org/query/",
-                "https://secondnode.neurobagel.org/query/",
-            ],
+            "(https://firstnode.neurobagel.org/query/, firstnode) (https://secondnode.neurobagel.org/query, secondnode)",
+            {
+                "https://firstnode.neurobagel.org/query/": "firstnode",
+                "https://secondnode.neurobagel.org/query/": "secondnode",
+            },
         ),
         (
-            " https://firstnode.neurobagel.org/query/ https://secondnode.neurobagel.org/query/  ",
-            [
-                "https://firstnode.neurobagel.org/query/",
-                "https://secondnode.neurobagel.org/query/",
-            ],
+            "(firstnode.neurobagel.org/query/, firstnode) (https://secondnode.neurobagel.org/query, secondnode)",
+            {
+                "https://secondnode.neurobagel.org/query/": "secondnode",
+            },
+        ),
+        (
+            "( , firstnode) (https://secondnode.neurobagel.org/query, secondnode)",
+            {
+                "https://secondnode.neurobagel.org/query/": "secondnode",
+            },
         ),
     ],
 )
-def test_parse_nodes_as_list(monkeypatch, set_nodes, expected_nodes):
+def test_parse_nodes_as_dict(monkeypatch, set_nodes, expected_nodes):
     """Test that Neurobagel node URLs provided in a string environment variable are correctly parseed into a list."""
     monkeypatch.setenv("NB_NODES", set_nodes)
     assert sorted(
-        util.parse_nodes_as_list(
+        util.parse_nodes_as_dict(
             os.environ.get("NB_NODES", "https://api.neurobagel.org/query/")
         )
     ) == sorted(expected_nodes)
