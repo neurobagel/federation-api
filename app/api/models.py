c4dc4eebe1a16dc1546bc16c918d68352946cc07
rmanaem <rmanaem@protonmail.ch>
2023-11-16 16:34:19 -0500

    Added `response_class` for `query` endpoint

--- app/api/models.py
+++ app/api/models.py
@@ -1,4 +1,7 @@
 """Data models."""
+
+from typing import Optional, Union
+
 from pydantic import BaseModel
 
 CONTROLLED_TERM_REGEX = r"^[a-zA-Z]+[:]\S+$"
@@ -15,3 +18,18 @@ class QueryModel(BaseModel):
     min_num_sessions: int = None
     assessment: str = None
     image_modal: str = None
+
+
+class CohortQueryResponse(BaseModel):
+    """Data model for query results for one matching dataset (i.e., a cohort)."""
+
+    node_name: str
+    dataset_uuid: str
+    # dataset_file_path: str  # TODO: Revisit this field once we have datasets without imaging info/sessions.
+    dataset_name: str
+    dataset_portal_uri: Optional[str]
+    dataset_total_subjects: int
+    records_protected: bool
+    num_matching_subjects: int
+    subject_data: Union[list[dict], str]
+    image_modals: list
