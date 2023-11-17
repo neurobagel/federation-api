f85c44b22266fa7ebece037bd624b880b4e85355
Arman Jahanpour <77515879+rmanaem@users.noreply.github.com>
2023-11-17 12:05:09 -0500

    [ENH] Implemented new endpoint to return known node APIs (#29)
    * Refactored and renamed `parse_nodes_as_list` to `parse_nodes_as_dict`
    
    * Updated `get` function and fixed `get_terms` function
    
    * Updated tests
    
    * Added `response_class` for `query` endpoint
    
    * Implemented endpoint to return all known node APIs
    
    * Updated response structure of `get_nodes` endpoint
    
    * Updated `get_nodes` endpoint
    
    * Addressed suggested changes from PR review

---
+++ app/api/routers/nodes.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from .. import utility as util
+
+router = APIRouter(prefix="/nodes", tags=["nodes"])
+
+
+@router.get("/")
+async def get_nodes():
+    """Returns a dict of all available nodes apis where key is node URL and value is node name."""
+    return [
+        {"NodeName": v, "ApiURL": k}
+        for k, v in util.parse_nodes_as_dict(util.NEUROBAGEL_NODES).items()
+    ]
