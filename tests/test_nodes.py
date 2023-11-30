fbc27268b735bd80d7da4b08c25466d1ae5dbbcc
Alyssa Dai <alyssa.ydai@gmail.com>
2023-11-30 00:19:39 -0500

    update local node setting in tests and test new warnings

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -7,14 +7,19 @@ from app.api import utility as util
 @pytest.mark.parametrize(
     "local_nodes",
     [
-        "(https://mylocalnode.org, Local Node)",
-        "(https://mylocalnode.org/, Local Node) (https://firstpublicnode.org/, First Public Node)",
+        {"https://mylocalnode.org/": "Local Node"},
+        {
+            "https://mylocalnode.org/": "Local Node",
+            "https://firstpublicnode.org/": "First Public Node",
+        },
     ],
 )
 def test_nodes_discovery_endpoint(test_app, monkeypatch, local_nodes):
     """Test that a federation node index is correctly created from locally set and remote node lists."""
-    monkeypatch.setattr(util, "LOCAL_NODES", local_nodes)
-  
+
+    def mock_parse_nodes_as_dict(path):
+        return local_nodes
+
     def mock_httpx_get(**kwargs):
         return httpx.Response(
             status_code=200,
@@ -30,6 +35,7 @@ def test_nodes_discovery_endpoint(test_app, monkeypatch, local_nodes):
             ],
         )
 
+    monkeypatch.setattr(util, "parse_nodes_as_dict", mock_parse_nodes_as_dict)
     monkeypatch.setattr(httpx, "get", mock_httpx_get)
 
     with test_app:
@@ -54,15 +60,16 @@ def test_nodes_discovery_endpoint(test_app, monkeypatch, local_nodes):
 
 def test_failed_public_nodes_fetching_raises_warning(test_app, monkeypatch):
     """Test that when request for remote list of public nodes fails, an informative warning is raised and the federation node index only includes local nodes."""
-    monkeypatch.setattr(
-        util, "LOCAL_NODES", "(https://mylocalnode.org, Local Node)"
-    )
+
+    def mock_parse_nodes_as_dict(path):
+        return {"https://mylocalnode.org/": "Local Node"}
 
     def mock_httpx_get(**kwargs):
         return httpx.Response(
             status_code=404, json={}, text="Some error message"
         )
 
+    monkeypatch.setattr(util, "parse_nodes_as_dict", mock_parse_nodes_as_dict)
     monkeypatch.setattr(httpx, "get", mock_httpx_get)
 
     with pytest.warns(UserWarning) as w:
@@ -78,8 +85,83 @@ def test_failed_public_nodes_fetching_raises_warning(test_app, monkeypatch):
                 }
             ]
 
+    assert len(w) == 1
     for warn_substr in [
         "Unable to fetch directory of public Neurobagel nodes",
-        "The federation API will only register the nodes defined locally for this API: {'https://mylocalnode.org/': 'Local Node'}",
+        "Federation will be limited to the nodes defined locally for this API: {'https://mylocalnode.org/': 'Local Node'}",
     ]:
         assert warn_substr in w[0].message.args[0]
+
+
+def test_unset_local_nodes_raises_warning(test_app, monkeypatch):
+    """Test that when no local nodes are set, an informative warning is raised and the federation node index only includes remote nodes."""
+
+    def mock_parse_nodes_as_dict(path):
+        return {}
+
+    def mock_httpx_get(**kwargs):
+        return httpx.Response(
+            status_code=200,
+            json=[
+                {
+                    "NodeName": "First Public Node",
+                    "ApiURL": "https://firstpublicnode.org",
+                },
+                {
+                    "NodeName": "Second Public Node",
+                    "ApiURL": "https://secondpublicnode.org",
+                },
+            ],
+        )
+
+    monkeypatch.setattr(util, "parse_nodes_as_dict", mock_parse_nodes_as_dict)
+    monkeypatch.setattr(httpx, "get", mock_httpx_get)
+
+    with pytest.warns(UserWarning) as w:
+        with test_app:
+            response = test_app.get("/nodes/")
+            assert util.FEDERATION_NODES == {
+                "https://firstpublicnode.org/": "First Public Node",
+                "https://secondpublicnode.org/": "Second Public Node",
+            }
+            assert response.json() == [
+                {
+                    "NodeName": "First Public Node",
+                    "ApiURL": "https://firstpublicnode.org/",
+                },
+                {
+                    "NodeName": "Second Public Node",
+                    "ApiURL": "https://secondpublicnode.org/",
+                },
+            ]
+
+    assert len(w) == 1
+    assert "No local Neurobagel nodes found" in w[0].message.args[0]
+
+
+def test_no_available_nodes_raises_error(monkeypatch, test_app):
+    """Test that when no local or remote nodes are available, an informative error is raised."""
+
+    def mock_parse_nodes_as_dict(path):
+        return {}
+
+    def mock_httpx_get(**kwargs):
+        return httpx.Response(
+            status_code=404, json={}, text="Some error message"
+        )
+
+    monkeypatch.setattr(util, "parse_nodes_as_dict", mock_parse_nodes_as_dict)
+    monkeypatch.setattr(httpx, "get", mock_httpx_get)
+
+    with pytest.warns(UserWarning) as w, pytest.raises(
+        RuntimeError
+    ) as exc_info:
+        with test_app:
+            pass
+
+    # Two warnings are expected, one for the failed GET request for public nodes, and one for the lack of local nodes.
+    assert len(w) == 2
+    assert (
+        "No local or public Neurobagel nodes available for federation"
+        in str(exc_info.value)
+    )
