1ea377aa1a243c20344c313b521f9c9c57426e0b
Alyssa Dai <alyssa.ydai@gmail.com>
2024-01-10 16:57:25 -0500

    test federated response given unreachable nodes, create fixture for single matching dataset result

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -1,6 +1,6 @@
 import httpx
 import pytest
-from fastapi import HTTPException, status
+from fastapi import status
 
 from app.api import utility as util
 
@@ -168,13 +168,12 @@ def test_no_available_nodes_raises_error(monkeypatch, test_app):
     )
 
 
-def test_partial_node_failures_are_handled_gracefully(
-    monkeypatch, test_app, capsys
+def test_partial_node_failure_responses_are_handled_gracefully(
+    monkeypatch, test_app, capsys, test_single_matching_dataset_result
 ):
     """
     Test that when queries to some nodes return errors, the overall API get request still succeeds,
-    the successful responses are returned along with a list of the encountered errors,
-    and the failed nodes are logged to the console.
+    the successful responses are returned along with a list of the encountered errors, and the failed nodes are logged to the console.
     """
     monkeypatch.setattr(
         util,
@@ -185,30 +184,17 @@ def test_partial_node_failures_are_handled_gracefully(
         },
     )
 
-    def mock_send_get_request(url, params):
-        if url == "https://firstpublicnode.org/query/":
-            return [
-                {
-                    "dataset_uuid": "http://neurobagel.org/vocab/12345",
-                    "dataset_name": "QPN",
-                    "dataset_portal_uri": "https://rpq-qpn.ca/en/researchers-section/databases/",
-                    "dataset_total_subjects": 200,
-                    "num_matching_subjects": 5,
-                    "records_protected": True,
-                    "subject_data": "protected",
-                    "image_modals": [
-                        "http://purl.org/nidash/nidm#T1Weighted",
-                        "http://purl.org/nidash/nidm#T2Weighted",
-                    ],
-                },
-            ]
+    def mock_httpx_get(**kwargs):
+        if kwargs["url"] == "https://firstpublicnode.org/query/":
+            return httpx.Response(
+                status_code=200, json=[test_single_matching_dataset_result]
+            )
 
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="500 Server Error: Internal Server Error",
+        return httpx.Response(
+            status_code=500, json={}, text="Some internal server error"
         )
 
-    monkeypatch.setattr(util, "send_get_request", mock_send_get_request)
+    monkeypatch.setattr(httpx, "get", mock_httpx_get)
 
     with pytest.warns(
         UserWarning,
@@ -222,22 +208,12 @@ def test_partial_node_failures_are_handled_gracefully(
         "errors": [
             {
                 "NodeName": "Second Public Node",
-                "error": "500 Server Error: Internal Server Error",
+                "error": "Internal Server Error: Some internal server error",
             },
         ],
         "responses": [
             {
-                "dataset_uuid": "http://neurobagel.org/vocab/12345",
-                "dataset_name": "QPN",
-                "dataset_portal_uri": "https://rpq-qpn.ca/en/researchers-section/databases/",
-                "dataset_total_subjects": 200,
-                "num_matching_subjects": 5,
-                "records_protected": True,
-                "subject_data": "protected",
-                "image_modals": [
-                    "http://purl.org/nidash/nidm#T1Weighted",
-                    "http://purl.org/nidash/nidm#T2Weighted",
-                ],
+                **test_single_matching_dataset_result,
                 "node_name": "First Public Node",
             },
         ],
@@ -246,3 +222,59 @@ def test_partial_node_failures_are_handled_gracefully(
     assert (
         "Queries to 1/2 nodes failed: ['Second Public Node']" in captured.out
     )
+
+
+def test_partial_node_connection_failures_are_handled_gracefully(
+    monkeypatch, test_app, capsys, test_single_matching_dataset_result
+):
+    """
+    Test that when requests to some nodes fail (e.g., if API is unreachable), the overall API get request still succeeds,
+    the successful responses are returned along with a list of the encountered errors, and the failed nodes are logged to the console.
+    """
+    monkeypatch.setattr(
+        util,
+        "FEDERATION_NODES",
+        {
+            "https://firstpublicnode.org/": "First Public Node",
+            "https://secondpublicnode.org/": "Second Public Node",
+        },
+    )
+
+    def mock_httpx_get(**kwargs):
+        if kwargs["url"] == "https://firstpublicnode.org/query/":
+            return httpx.Response(
+                status_code=200, json=[test_single_matching_dataset_result]
+            )
+
+        raise httpx.ConnectError("Some connection error")
+
+    monkeypatch.setattr(httpx, "get", mock_httpx_get)
+
+    with pytest.warns(
+        UserWarning,
+        match=r"Second Public Node \(https://secondpublicnode.org/\) did not succeed",
+    ):
+        response = test_app.get("/query/")
+        captured = capsys.readouterr()
+
+    assert response.status_code == status.HTTP_207_MULTI_STATUS
+    assert response.json() == {
+        "errors": [
+            {
+                "NodeName": "Second Public Node",
+                "error": "Request failed due to a network error or because the node API cannot be reached: Some connection error",
+            },
+        ],
+        "responses": [
+            {
+                **test_single_matching_dataset_result,
+                "node_name": "First Public Node",
+            },
+        ],
+        "nodes_response_status": "partial success",
+    }
+    assert (
+        "Queries to 1/2 nodes failed: ['Second Public Node']" in captured.out
+    )
+
+    # TODO: test for all nodes failed, or succeeded
