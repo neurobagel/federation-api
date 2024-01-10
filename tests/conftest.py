e3385991707181cf772281af1630e6b7578ef109
Alyssa Dai <alyssa.ydai@gmail.com>
2024-01-10 17:01:54 -0500

    refactor tests of federated query response into separate module

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,21 +8,3 @@ from app.main import app
 def test_app():
     client = TestClient(app)
     yield client
-
-
-@pytest.fixture()
-def test_single_matching_dataset_result():
-    """Valid aggregate query result for a single matching dataset."""
-    return {
-        "dataset_uuid": "http://neurobagel.org/vocab/12345",
-        "dataset_name": "QPN",
-        "dataset_portal_uri": "https://rpq-qpn.ca/en/researchers-section/databases/",
-        "dataset_total_subjects": 200,
-        "num_matching_subjects": 5,
-        "records_protected": True,
-        "subject_data": "protected",
-        "image_modals": [
-            "http://purl.org/nidash/nidm#T1Weighted",
-            "http://purl.org/nidash/nidm#T2Weighted",
-        ],
-    }
