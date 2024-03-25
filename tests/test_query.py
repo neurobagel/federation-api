19cefb9378f78e71b1b34db0b068b542a12bb399
Sebastian Urchs <sebastian.urchs@gmail.com>
2024-03-25 16:26:46 -0400

    [REF] Refactor tests to use logging

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 import httpx
 import pytest
@@ -26,9 +27,9 @@ def mocked_single_matching_dataset_result():
 def test_partial_node_failure_responses_handled_gracefully(
     monkeypatch,
     test_app,
-    capsys,
     set_valid_test_federation_nodes,
     mocked_single_matching_dataset_result,
+    caplog,
 ):
     """
     Test that when queries to some nodes return unsuccessful responses, the overall API get request still succeeds,
@@ -54,7 +55,6 @@ def test_partial_node_failure_responses_handled_gracefully(
         match=r"Second Public Node \(https://secondpublicnode.org/\) did not succeed",
     ):
         response = test_app.get("/query/")
-        captured = capsys.readouterr()
 
     assert response.status_code == status.HTTP_207_MULTI_STATUS
     assert response.json() == {
@@ -73,7 +73,7 @@ def test_partial_node_failure_responses_handled_gracefully(
         "nodes_response_status": "partial success",
     }
     assert (
-        "Requests to 1/2 nodes failed: ['Second Public Node']" in captured.out
+        "Requests to 1/2 nodes failed: ['Second Public Node']" in caplog.text
     )
 
 
@@ -102,11 +102,11 @@ def test_partial_node_failure_responses_handled_gracefully(
 def test_partial_node_request_failures_handled_gracefully(
     monkeypatch,
     test_app,
-    capsys,
     set_valid_test_federation_nodes,
     mocked_single_matching_dataset_result,
     error_to_raise,
     expected_node_message,
+    caplog,
 ):
     """
     Test that when requests to some nodes fail (so there is no response status code), the overall API get request still succeeds,
@@ -128,7 +128,6 @@ def test_partial_node_request_failures_handled_gracefully(
         match=r"Second Public Node \(https://secondpublicnode.org/\) did not succeed",
     ):
         response = test_app.get("/query/")
-        captured = capsys.readouterr()
 
     assert response.status_code == status.HTTP_207_MULTI_STATUS
 
@@ -146,7 +145,7 @@ def test_partial_node_request_failures_handled_gracefully(
     assert node_errors[0]["node_name"] == "Second Public Node"
     assert expected_node_message in node_errors[0]["error"]
     assert (
-        "Requests to 1/2 nodes failed: ['Second Public Node']" in captured.out
+        "Requests to 1/2 nodes failed: ['Second Public Node']" in caplog.text
     )
 
 
@@ -155,7 +154,7 @@ def test_all_nodes_failure_handled_gracefully(
     test_app,
     mock_failed_connection_httpx_get,
     set_valid_test_federation_nodes,
-    capsys,
+    caplog,
 ):
     """
     Test that when queries sent to all nodes fail, the federation API get request still succeeds,
@@ -169,7 +168,6 @@ def test_all_nodes_failure_handled_gracefully(
         UserWarning,
     ) as w:
         response = test_app.get("/query/")
-        captured = capsys.readouterr()
 
     assert len(w) == 2
     assert response.status_code == status.HTTP_207_MULTI_STATUS
@@ -180,20 +178,23 @@ def test_all_nodes_failure_handled_gracefully(
     assert response["responses"] == []
     assert (
         "Requests to 2/2 nodes failed: ['First Public Node', 'Second Public Node']"
-        in captured.out
+        in caplog.text
     )
 
 
 def test_all_nodes_success_handled_gracefully(
     monkeypatch,
     test_app,
-    capsys,
+    caplog,
     set_valid_test_federation_nodes,
     mocked_single_matching_dataset_result,
 ):
     """
     Test that when queries sent to all nodes succeed, the federation API response includes an overall success status and no errors.
     """
+    # Need to set the logging level to INFO so that the success message is captured
+    # pytest by default captures WARNING or above: https://docs.pytest.org/en/stable/how-to/logging.html#caplog-fixture
+    caplog.set_level(logging.INFO)
 
     async def mock_httpx_get(self, **kwargs):
         return httpx.Response(
@@ -203,7 +204,6 @@ def test_all_nodes_success_handled_gracefully(
     monkeypatch.setattr(httpx.AsyncClient, "get", mock_httpx_get)
 
     response = test_app.get("/query/")
-    captured = capsys.readouterr()
 
     assert response.status_code == status.HTTP_200_OK
 
@@ -211,4 +211,4 @@ def test_all_nodes_success_handled_gracefully(
     assert response["nodes_response_status"] == "success"
     assert response["errors"] == []
     assert len(response["responses"]) == 2
-    assert "Requests to all nodes succeeded (2/2)" in captured.out
+    assert "Requests to all nodes succeeded (2/2)" in caplog.text
