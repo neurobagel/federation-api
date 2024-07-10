5893ded41e4b03e096ea0d8f7c43f368ba78a23a
Alyssa Dai <alyssa.ydai@gmail.com>
2024-07-09 22:35:25 -0400

    add implicit OAuth flow + token verification via Google for /query route

--- app/api/routers/query.py
+++ app/api/routers/query.py
@@ -1,11 +1,47 @@
 """Router for query path operations."""
 
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi.security import OAuth2
+from fastapi.security.utils import get_authorization_scheme_param
+from google.auth.transport import requests
+from google.oauth2 import id_token
 
 from .. import crud
 from ..models import CombinedQueryResponse, QueryModel
 
 router = APIRouter(prefix="/query", tags=["query"])
+
+oauth2_scheme = OAuth2(
+    flows={
+        "implicit": {
+            "authorizationUrl": "https://accounts.google.com/o/oauth2/auth",
+        }
+    }
+)
+
+CLIENT_ID = (
+    "465352721782-aj7eam9jdu967adj8vd8ckih325k62d5.apps.googleusercontent.com"
+)
+
+
+def verify_token(token: str):
+    """Verify the Google ID token. Raise an HTTPException if the token is invalid."""
+    # Adapted from https://developers.google.com/identity/gsi/web/guides/verify-google-id-token#python
+    try:
+        # Extract the token from the "Bearer" scheme
+        # (See https://github.com/tiangolo/fastapi/blob/master/fastapi/security/oauth2.py#L473-L485)
+        _, param = get_authorization_scheme_param(token)
+        id_info = id_token.verify_oauth2_token(
+            param, requests.Request(), CLIENT_ID
+        )
+        # TODO: Remove print statement - just for testing
+        print("Token verified: ", id_info)
+    except ValueError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Invalid token: {exc}",
+            headers={"WWW-Authenticate": "Bearer"},
+        ) from exc
 
 
 # We use the Response parameter below to change the status code of the response while still being able to validate the returned data using the response model.
@@ -16,9 +52,28 @@ router = APIRouter(prefix="/query", tags=["query"])
 # example responses for different status codes in the OpenAPI docs (less relevant for now since there is only one response model).
 @router.get("/", response_model=CombinedQueryResponse)
 async def get_query(
-    response: Response, query: QueryModel = Depends(QueryModel)
+    response: Response,
+    query: QueryModel = Depends(QueryModel),
+    token: str = Depends(oauth2_scheme),
 ):
     """When a GET request is sent, return list of dicts corresponding to subject-level metadata aggregated by dataset."""
+    # NOTE: Currently, when the request is unauthenticated (missing or malformed authorization header -> missing token),
+    # the default response is a 403 Forbidden error.
+    # This doesn't fully align with HTTP status code conventions:
+    # - 401 Unauthorized should be used when the client lacks authentication credentials
+    # - 403 Forbidden should be used when the client has been authenticated but lacks the required permissions
+    # If we really care about returning a 401 Unauthorized error, we can use auto_error=False
+    # when creating the OAuth2 object and raise a custom HTTPException.
+    # See also https://github.com/tiangolo/fastapi/discussions/9130
+    # if not token:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_401_UNAUTHORIZED,
+    #         detail="Not authenticated",
+    #         headers={"WWW-Authenticate": "Bearer"},
+    #     )
+
+    verify_token(token)
+
     response_dict = await crud.get(
         query.min_age,
         query.max_age,
