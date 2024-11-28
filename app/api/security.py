3e532a500dea08b8483fc159dabb8ea8eb29b3d7
Alyssa Dai <alyssa.ydai@gmail.com>
2024-11-28 14:36:53 -0500

    manually verify ID token using PyJWT instead of google_auth

--- app/api/security.py
+++ app/api/security.py
@@ -1,10 +1,9 @@
 import os
 
+import jwt
 from fastapi import HTTPException, status
 from fastapi.security.utils import get_authorization_scheme_param
-from google.auth.exceptions import GoogleAuthError
-from google.auth.transport import requests
-from google.oauth2 import id_token
+from jwt import PyJWKClient, PyJWTError
 
 AUTH_ENABLED = os.environ.get("NB_ENABLE_AUTH", "True").lower() == "true"
 CLIENT_ID = os.environ.get("NB_QUERY_CLIENT_ID", None)
@@ -27,19 +26,36 @@ def verify_and_extract_token(token: str) -> str:
     Verify and return the Google ID token with the authorization scheme stripped.
     Raise an HTTPException if the token is invalid.
     """
+    google_keys_url = "https://www.googleapis.com/oauth2/v3/certs"
+
     # Adapted from https://developers.google.com/identity/gsi/web/guides/verify-google-id-token#python
     try:
         # Extract the token from the "Bearer" scheme
         # (See https://github.com/tiangolo/fastapi/blob/master/fastapi/security/oauth2.py#L473-L485)
         # TODO: Check also if scheme of token is "Bearer"?
         _, extracted_token = get_authorization_scheme_param(token)
-        id_info = id_token.verify_oauth2_token(
-            extracted_token, requests.Request(), CLIENT_ID
+
+        # Determine which key was used to sign the token
+        # Adapted from https://pyjwt.readthedocs.io/en/stable/usage.html#retrieve-rsa-signing-keys-from-a-jwks-endpoint
+        jwks_client = PyJWKClient(google_keys_url)
+        signing_key = jwks_client.get_signing_key_from_jwt(extracted_token)
+
+        # https://pyjwt.readthedocs.io/en/stable/api.html#jwt.decode
+        id_info = jwt.decode(
+            jwt=extracted_token,
+            key=signing_key,
+            options={
+                "verify_signature": True,
+                "require": ["aud", "iss", "exp", "iat", "nbf"],
+            },
+            audience=CLIENT_ID,
+            issuer=["https://accounts.google.com", "accounts.google.com"],
         )
+
         # TODO: Remove print statement or turn into logging
         print("Token verified: ", id_info)
         return extracted_token
-    except (GoogleAuthError, ValueError) as exc:
+    except (PyJWTError, ValueError) as exc:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"Invalid token: {exc}",
