a343df84e960ca372afd9997a7c821cded77e276
Alyssa Dai <alyssa.ydai@gmail.com>
2024-11-29 17:29:35 -0500

    add positive unit test for verify_token

--- tests/test_security.py
+++ tests/test_security.py
@@ -37,8 +37,10 @@ def test_missing_client_id_ignored_when_auth_disabled(monkeypatch, test_app):
     "invalid_token",
     ["Bearer faketoken", "Bearer", "faketoken", "fakescheme faketoken"],
 )
-def test_invalid_token_raises_error(invalid_token):
+def test_invalid_token_raises_error(monkeypatch, invalid_token):
     """Test that an invalid token raises an error from the verification process."""
+    monkeypatch.setattr("app.api.security.CLIENT_ID", "foo.id")
+
     with pytest.raises(HTTPException) as exc_info:
         verify_token(invalid_token)
 
@@ -77,3 +79,32 @@ def test_token_returned_without_auth_scheme(monkeypatch, enable_auth):
     """
     mock_valid_token = "Bearer foo"
     assert extract_token(mock_valid_token) == "foo"
+
+
+def test_valid_token_does_not_error_out(monkeypatch, enable_auth):
+    """
+    Test that when a valid token is passed to verify_token, the token is returned without errors.
+    """
+
+    def mock_get_signing_key_from_jwt(*args, **kwargs):
+        # NOTE: The actual get_signing_key_from_jwt method should return a key object
+        return "signingkey"
+
+    def mock_jwt_decode(*args, **kwargs):
+        return {
+            "iss": "https://myissuer.com",
+            "aud": "123abc.myapp.com",
+            "sub": "1234567890",
+            "name": "John Doe",
+            "iat": 1730476922,
+            "exp": 1730480522,
+        }
+
+    monkeypatch.setattr("app.api.security.CLIENT_ID", "123abc.myapp.com")
+    monkeypatch.setattr(
+        "app.api.security.JWKS_CLIENT.get_signing_key_from_jwt",
+        mock_get_signing_key_from_jwt,
+    )
+    monkeypatch.setattr("app.api.security.jwt.decode", mock_jwt_decode)
+
+    assert verify_token("Bearer myvalidtoken") == "myvalidtoken"
