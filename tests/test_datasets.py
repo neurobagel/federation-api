f507cacc8b9d3e129c6566158ac2c45ec7e24322
Alyssa Dai <alyssa.ydai@gmail.com>
2026-01-08 15:48:32 -0500

    separate tests of POST /subjects and /datasets response status vs. results content

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -16,7 +16,7 @@ ROUTE = "/datasets"
         None,
     ],
 )
-def test_valid_nodes_do_not_error(
+def test_valid_nodes_query_does_not_error(
     test_app,
     disable_auth,
     set_valid_test_federation_nodes,
@@ -26,7 +26,7 @@ def test_valid_nodes_do_not_error(
     caplog,
 ):
     """
-    Smoke test that when a valid 'nodes' list is provided, POST /datasets does not raise an error and returns a combined response with the correct fields.
+    Smoke test that when a valid 'nodes' list is provided, POST /datasets does not raise an error and returns a successful combined response.
     """
 
     async def mock_httpx_request(self, method, url, **kwargs):
@@ -45,6 +45,51 @@ def test_valid_nodes_do_not_error(
     assert response["errors"] == []
     assert len(response["responses"]) == 2
     assert "Requests to all nodes succeeded (2/2)" in caplog.text
+
+
+@pytest.mark.parametrize(
+    "valid_nodes",
+    [
+        [
+            {
+                "node_url": "https://firstpublicnode.org/",
+                "dataset_uuids": [
+                    "http://neurobagel.org/vocab/12345",
+                    "http://neurobagel.org/vocab/67890",
+                ],
+            },
+            {
+                "node_url": "https://secondpublicnode.org/",
+            },
+        ],
+        [],
+        None,
+    ],
+)
+def test_valid_nodes_query_returns_only_dataset_metadata(
+    test_app,
+    disable_auth,
+    set_valid_test_federation_nodes,
+    mocked_datasets_query_response_for_single_dataset,
+    valid_nodes,
+    monkeypatch,
+):
+    """
+    Test that when a valid 'nodes' list is provided, the POST /datasets response includes expected
+    dataset-level metadata fields and no subject data.
+    """
+
+    async def mock_httpx_request(self, method, url, **kwargs):
+        return httpx.Response(
+            status_code=200,
+            json=[mocked_datasets_query_response_for_single_dataset],
+        )
+
+    monkeypatch.setattr(httpx.AsyncClient, "request", mock_httpx_request)
+
+    response = test_app.post(ROUTE, json={"nodes": valid_nodes})
+
+    response = response.json()
     for response in response["responses"]:
         assert "subject_data" not in response
         assert "dataset_name" in response
@@ -78,7 +123,7 @@ def test_valid_nodes_do_not_error(
         ),
     ],
 )
-def test_invalid_nodes_raise_error(
+def test_invalid_nodes_query_raises_error(
     test_app,
     disable_auth,
     set_valid_test_federation_nodes,
