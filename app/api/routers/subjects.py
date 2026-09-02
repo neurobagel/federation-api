713d5d91e4f6370aaa3cf5995b17b3d8afd059bb
Alyssa Dai <alyssa.ydai@gmail.com>
2026-09-02 18:19:45 -0400

    move comments from query.py to subjects.py

--- app/api/routers/subjects.py
+++ app/api/routers/subjects.py
@@ -9,6 +9,9 @@ from ..security import verify_token
 
 router = APIRouter(prefix="/subjects", tags=["subjects"])
 
+# Adapted from info in https://github.com/tiangolo/fastapi/discussions/9137#discussioncomment-5157382
+# I believe for us this is purely for documentatation/a nice looking interactive API docs page,
+# and doesn't actually have any bearing on the ID token validation process.
 oauth2_scheme = OAuth2(
     flows={
         "implicit": {
@@ -22,6 +25,10 @@ oauth2_scheme = OAuth2(
 
 # We use the Response parameter below to change the status code of the response while still being able to validate the returned data using the response model.
 # (see https://fastapi.tiangolo.com/advanced/response-change-status-code/ for more info).
+#
+# TODO: if our response model for fully successful vs. not fully successful responses grows more complex in the future,
+# consider additionally using https://fastapi.tiangolo.com/advanced/additional-responses/#additional-response-with-model to document
+# example responses for different status codes in the OpenAPI docs (less relevant for now since there is only one response model).
 @router.post("", response_model=CombinedSubjectsQueryResponse)
 async def post_subjects_query(
     response: Response,
@@ -29,6 +36,20 @@ async def post_subjects_query(
     token: str | None = Depends(oauth2_scheme),
 ):
     """When a POST request is sent, return list of dicts corresponding to subject-level metadata aggregated by dataset."""
+    # NOTE: Currently, when the request is unauthenticated (missing or malformed authorization header -> missing token),
+    # the default response is a 403 Forbidden error.
+    # This doesn't fully align with HTTP status code conventions:
+    # - 401 Unauthorized should be used when the client lacks authentication credentials
+    # - 403 Forbidden should be used when the client has been authenticated but lacks the required permissions
+    # If we really care about returning a 401 Unauthorized error, we can use auto_error=False
+    # when creating the OAuth2 object and raise a custom HTTPException.
+    # See also https://github.com/tiangolo/fastapi/discussions/9130
+    # if not token:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_401_UNAUTHORIZED,
+    #         detail="Not authenticated",
+    #         headers={"WWW-Authenticate": "Bearer"},
+    #     )
     if security.AUTH_ENABLED:
         if token is None:
             raise HTTPException(
@@ -37,6 +58,9 @@ async def post_subjects_query(
             )
         token = verify_token(token)
 
+    # Remove fields set to None (default value) from the dict
+    # to avoid type validation errors of specific query parameters on the receiving nodes
+    # (e.g., the value an n-API receives for min_age must be a float and cannot be null/None)
     response_dict = await crud.post_subjects(
         query=query.model_dump(exclude_none=True),
         token=token,
